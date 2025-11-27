d806150861e7345f8e9dc34c941dc9751f7da12b
Hariram4428 <hariram.subramanian@syncfusion.com>
2025-11-27 18:29:46 +0530

    MAUI Updated blog sample

---
+++ PickersSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace PickersSample.ViewModels
+{
+    public class MainViewModel
+    {
+        public ICommand NavigateDateTimePickerCommand { get; }
+        public ICommand NavigatePickerCommand { get; }
+        public ICommand NavigateDatePickerCommand { get; }
+        public ICommand NavigateTimePickerCommand { get; }
+
+        public MainViewModel()
+        {
+            NavigateDateTimePickerCommand = new Command(async () => await Shell.Current.GoToAsync("//DateTimePickersPage"));
+            NavigatePickerCommand = new Command(async () => await Shell.Current.GoToAsync("//PickerPage"));
+            NavigateDatePickerCommand = new Command(async () => await Shell.Current.GoToAsync("//DatePickerPage"));
+            NavigateTimePickerCommand = new Command(async () => await Shell.Current.GoToAsync("//TimePickerPage"));
+        }
+    }
+}
